935be57d49125f85da60c21f457aaadc35297e37
magarich228 <kirillgroshev52@gmail.com>
2024-10-27 22:55:30 +0300

    configuration added.

--- src/AvatariaCheats.CLI/Program.cs
+++ src/AvatariaCheats.CLI/Program.cs
@@ -1,72 +1,43 @@
 ﻿using AvatariaCheats.CLI;
+using Microsoft.Extensions.Configuration;
 using Point = AvatariaCheats.CLI.Point;
+
+ConfigurationBuilder builder = new ConfigurationBuilder();
+builder.AddJsonFile("appsettings.json", false, true);
+
+var configuration = builder.Build();
+
+var point1 = new Point();
+var point2 = new Point();
+var point3 = new Point();
+var point4 = new Point();
+
+configuration.Bind("Point1", point1);
+configuration.Bind("Point2", point2);
+configuration.Bind("Point3", point3);
+configuration.Bind("Point4", point4);
 
 var keyInvoker = new KeyInvoker();
 var obs = new ColorObserver();
 
-var point1 = new Point(){ X = 751, Y = 870 };
-var point2 = new Point(){ X = 900, Y = 860 };
-var point3 = new Point(){ X = 1000, Y = 840 };
-var point4 = new Point(){ X = 1170, Y = 870 };
-
-var t1 = Task.Run(() =>
-{
-    while (true)
-    {
-        var point = point1;
-    
-        if (obs.GetPixelColor(point.X, point.Y))
-        {
-            keyInvoker.Invoke(Keys.A);
-            Console.WriteLine($"{point.X} {point.Y} A");
-            Task.Delay(100).GetAwaiter().GetResult();
-        }
-    }
-});
-
-var t2 = Task.Run(() =>
-{
-    while (true)
-    {
-        var point = point2;
-    
-        if (obs.GetPixelColor(point.X, point.Y))
-        {
-            keyInvoker.Invoke(Keys.S);
-            Console.WriteLine($"{point.X} {point.Y} S");
-            Task.Delay(100).GetAwaiter().GetResult();
-        }
-    }
-});
-
-var t3 = Task.Run(() =>
-{
-    while (true)
-    {
-        var point = point3;
-    
-        if (obs.GetPixelColor(point.X, point.Y))
-        {
-            keyInvoker.Invoke(Keys.W);
-            Console.WriteLine($"{point.X} {point.Y} W");
-            Task.Delay(100).GetAwaiter().GetResult();
-        }
-    }
-});
-
-var t4 = Task.Run(() =>
-{
-    while (true)
-    {
-        var point = point4;
-    
-        if (obs.GetPixelColor(point.X, point.Y))
-        {
-            keyInvoker.Invoke(Keys.D);
-            Console.WriteLine($"{point.X} {point.Y} D");
-            Task.Delay(100).GetAwaiter().GetResult();
-        }
-    }
-});
+var t1 = Task.Run(() => ProcessAsync(obs, keyInvoker, point1, Keys.A));
+var t2 = Task.Run(() => ProcessAsync(obs, keyInvoker, point2, Keys.S));
+var t3 = Task.Run(() => ProcessAsync(obs, keyInvoker, point3, Keys.W));
+var t4 = Task.Run(() => ProcessAsync(obs, keyInvoker, point4, Keys.D));
 
 Task.WhenAll(t1, t2, t3, t4).GetAwaiter().GetResult();
+
+static async Task ProcessAsync(ColorObserver obs, KeyInvoker keyInvoker, Point point, Keys key)
+{
+    while (true)
+    {
+        if (obs.GetPixelColor(point.X, point.Y))
+        {
+            keyInvoker.Invoke(key);
+            
+            Console.WriteLine($"{point.X} {point.Y} {key}");
+
+            await Task.Delay(100);
+        }
+    }
+}
